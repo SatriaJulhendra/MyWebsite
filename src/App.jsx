b23a42067508dd449c025657d163ce39fa59fb6d
Satria Julhendra <satria.learn07@gmail.com>
2023-12-22 02:55:04 +0700

    1.0.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {Routes, Route} from "react-router-dom"
 
-import NavbarComponent from "./components/NavbarComponent"
+import NavbarComponent from "./Components/NavbarComponent"
 import FooterComponent from "./Components/FooterComponent"
 
 import HomePage from "./Pages/HomePage"
