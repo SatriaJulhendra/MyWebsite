79d11349fd1eb3b687f4ee249d40d03f09d564ff
Satria Julhendra <satria.learn07@gmail.com>
2025-02-28 09:41:41 +0700

    update Projects

--- src/data/index.js
+++ src/data/index.js
@@ -1,8 +1,6 @@
 import Project1 from "../assets/Project Portfolio/Project1.png";
 import Project2 from "../assets/Project Portfolio/Project2.png";
 import Project3 from "../assets/Project Portfolio/Project3.png";
-import Project4 from "../assets/Project Portfolio/Project4.png";
-import Project5 from "../assets/Project Portfolio/Project5.png";
 import Project6 from "../assets/Project Portfolio/Project6.png";
 
 import Sertifikat1 from "../assets/Sertifikat/Sertifikat1.png";
@@ -78,22 +76,10 @@ export const projects = [
         id:3,
         name: "Website Adminimistrasi UPT Puskesmas Sapta Taruna",
         img : Project3,
-        link : "admin-puskesmas-4pgqp830q-satria-julhendras-projects.vercel.app",
+        link : "admin-puskesmas.vercel.app",
     },
     {
         id:4,
-        name: "Skill Test Internship Detikcom",
-        img : Project4,
-        link : "update",
-    },
-    {
-        id:5,
-        name: "Test Frontend Developer CTARSA Foundation",
-        img : Project5,
-        link : "update",
-    },
-    {
-        id:6,
         name: "Website E-Learning ",
         img : Project6,
         link : "https://satria-j-e-learning.vercel.app/",
